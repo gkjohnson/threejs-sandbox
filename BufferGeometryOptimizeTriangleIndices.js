ab1b76ab7f755f328c4106bc51ae00b98370e8cf
Garrett Johnson <garrett.kjohnson@gmail.com>
2018-05-22 21:00:44 -0700

    variable name updates

--- BufferGeometryOptimizeTriangleIndices.js
+++ BufferGeometryOptimizeTriangleIndices.js
@@ -1,22 +1,25 @@
 THREE.BufferGeometry.prototype.optimizeTriangleIndices = function( precision = 3 ) {
 
-	var map = {};
+	var hashToIndex = {};
 	var indices = this.getIndex();
-	var len = indices ? indices.count : this.getAttribute( 'position' ).count;
-	var names = Object.keys( this.attributes );
+	var positions = this.getAttribute( 'position' );
+	var vertexCount = indices ? indices.count : positions.count;
+	
 	var newIndices = [];
 	var currIndex = 0;
+	
+	var attributeNames = Object.keys( this.attributes );
 	var attrArrays = {};
 
 	var multVal = Math.pow( 10, precision + 1 );
-	for ( var i = 0; i < len; i ++ ) {
+	for ( var i = 0; i < vertexCount; i ++ ) {
 
 		// Generate a hash for the vertex attributes at the current index 'i'
 		var index = indices ? indices.array[ i ] : i;
 		var hash = '';
-		for ( var j = 0, l = names.length; j < l; j ++ ) {
+		for ( var j = 0, l = attributeNames.length; j < l; j ++ ) {
 
-			var name = names[ j ];
+			var name = attributeNames[ j ];
 			var attribute = this.getAttribute( name );
 			var size = attribute.itemSize;
 			var array = attribute.array;
@@ -32,16 +35,16 @@ THREE.BufferGeometry.prototype.optimizeTriangleIndices = function( precision = 3
 		}
 
 		// Add another reference to the vertex if it's already referenced
-		if ( hash in map ) {
+		if ( hash in hashToIndex ) {
 
-			newIndices.push( map[ hash ] );
+			newIndices.push( hashToIndex[ hash ] );
 
 		} else {
 
 			// copy data to the new index in the attribute arrays
-			for ( var j = 0, l = names.length; j < l; j ++ ) {
+			for ( var j = 0, l = attributeNames.length; j < l; j ++ ) {
 
-				var name = names[ j ];				
+				var name = attributeNames[ j ];				
 				var attribute = this.getAttribute( name );
 				var array = attribute.array;
 				var size = attribute.itemSize;
@@ -58,16 +61,16 @@ THREE.BufferGeometry.prototype.optimizeTriangleIndices = function( precision = 3
 
 			}
 
-			map[ hash ] = currIndex;
+			hashToIndex[ hash ] = currIndex;
 			newIndices.push(currIndex);
 			currIndex ++;
 		}
 
 	}
 
-	for ( var i = 0, l = names.length; i < l; i ++ ) {
+	for ( var i = 0, l = attributeNames.length; i < l; i ++ ) {
 
-		var name = names[ i ];
+		var name = attributeNames[ i ];
 		var attribute = this.getAttribute( name );
 		var buffer = new attribute.array.constructor( attrArrays[ name ] );
 
