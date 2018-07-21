21dfe729ac79096283f1d56cf9cf16407dd2cfb7
Garrett Johnson <garrett.kjohnson@gmail.com>
2018-07-21 16:02:06 -0700

    add basic shader

--- screenSpaceReflectionsPass/SSRRPass.js
+++ screenSpaceReflectionsPass/SSRRPass.js
@@ -40,6 +40,8 @@ THREE.SSRRPass = function ( scene, camera, options = {} ) {
 		} );
 	this._normalBuffer.texture.name = "SSRRPass.Normal";
 	this._normalBuffer.texture.generateMipmaps = false;
+	this._normalMaterialPool = [];
+	this._nextMaterialIndex = 0;
 
 	this._compositeCamera = new THREE.OrthographicCamera( - 1, 1, 1, - 1, 0, 1 );
 	this._compositeScene = new THREE.Scene();
@@ -69,9 +71,6 @@ THREE.SSRRPass.prototype = Object.assign( Object.create( THREE.Pass.prototype ),
 	},
 
 	getNormalMaterial: function ( material ) {
-
-		this._normalMaterialPool = this._normalMaterialPool || [];
-		this._nextMaterialIndex = this._nextMaterialIndex || 0;
 
 		if ( this._nextMaterialIndex >= this._normalMaterial.length ) {
 
@@ -120,6 +119,141 @@ THREE.SSRRPass.prototype = Object.assign( Object.create( THREE.Pass.prototype ),
 
 		this.resetMaterialPool();
 
+	},
+
+	createMaterial() {
+
+		return new THREE.ShaderMaterial( {
+
+			uniforms: {
+
+				roughnessMap: { value: null },
+				roughness: { value: 0 },
+
+				metalnessMap: { value: null },
+				metalness: { value: 0 },
+
+				normalMap: { value: null }
+
+			},
+
+			vertexShader: `
+				#define PHYSICAL
+				varying vec3 vViewPosition;
+				#ifndef FLAT_SHADED
+					varying vec3 vNormal;
+				#endif
+				#include <common>
+				#include <uv_pars_vertex>
+				#include <uv2_pars_vertex>
+				#include <displacementmap_pars_vertex>
+				#include <color_pars_vertex>
+				#include <fog_pars_vertex>
+				#include <morphtarget_pars_vertex>
+				#include <skinning_pars_vertex>
+				#include <shadowmap_pars_vertex>
+				#include <logdepthbuf_pars_vertex>
+				#include <clipping_planes_pars_vertex>
+				void main() {
+					#include <uv_vertex>
+					#include <uv2_vertex>
+					#include <color_vertex>
+					#include <beginnormal_vertex>
+					#include <morphnormal_vertex>
+					#include <skinbase_vertex>
+					#include <skinnormal_vertex>
+					#include <defaultnormal_vertex>
+				#ifndef FLAT_SHADED
+					vNormal = normalize( transformedNormal );
+				#endif
+					#include <begin_vertex>
+					#include <morphtarget_vertex>
+					#include <skinning_vertex>
+					#include <displacementmap_vertex>
+					#include <project_vertex>
+					#include <logdepthbuf_vertex>
+					#include <clipping_planes_vertex>
+					vViewPosition = - mvPosition.xyz;
+					#include <worldpos_vertex>
+					#include <shadowmap_vertex>
+					#include <fog_vertex>
+				}
+			`,
+
+			fragmentShader: `
+				#define PHYSICAL
+				uniform vec3 diffuse;
+				uniform vec3 emissive;
+				uniform float roughness;
+				uniform float metalness;
+				uniform float opacity;
+				#ifndef STANDARD
+					uniform float clearCoat;
+					uniform float clearCoatRoughness;
+				#endif
+				varying vec3 vViewPosition;
+				#ifndef FLAT_SHADED
+					varying vec3 vNormal;
+				#endif
+				#include <common>
+				#include <packing>
+				#include <dithering_pars_fragment>
+				#include <color_pars_fragment>
+				#include <uv_pars_fragment>
+				#include <uv2_pars_fragment>
+				#include <map_pars_fragment>
+				#include <alphamap_pars_fragment>
+				#include <aomap_pars_fragment>
+				#include <lightmap_pars_fragment>
+				#include <emissivemap_pars_fragment>
+				#include <envmap_pars_fragment>
+				#include <fog_pars_fragment>
+				#include <bsdfs>
+				#include <cube_uv_reflection_fragment>
+				#include <lights_pars_begin>
+				#include <lights_pars_maps>
+				#include <lights_physical_pars_fragment>
+				#include <shadowmap_pars_fragment>
+				#include <bumpmap_pars_fragment>
+				#include <normalmap_pars_fragment>
+				#include <roughnessmap_pars_fragment>
+				#include <metalnessmap_pars_fragment>
+				#include <logdepthbuf_pars_fragment>
+				#include <clipping_planes_pars_fragment>
+				void main() {
+					#include <clipping_planes_fragment>
+					vec4 diffuseColor = vec4( diffuse, opacity );
+					ReflectedLight reflectedLight = ReflectedLight( vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ) );
+					vec3 totalEmissiveRadiance = emissive;
+					#include <logdepthbuf_fragment>
+					#include <map_fragment>
+					#include <color_fragment>
+					#include <alphamap_fragment>
+					#include <alphatest_fragment>
+					#include <roughnessmap_fragment>
+					#include <metalnessmap_fragment>
+					#include <normal_fragment_begin>
+					#include <normal_fragment_maps>
+					#include <emissivemap_fragment>
+					#include <lights_physical_fragment>
+					#include <lights_fragment_begin>
+					#include <lights_fragment_maps>
+					#include <lights_fragment_end>
+					#include <aomap_fragment>
+					vec3 outgoingLight = reflectedLight.directDiffuse + reflectedLight.indirectDiffuse + reflectedLight.directSpecular + reflectedLight.indirectSpecular + totalEmissiveRadiance;
+					gl_FragColor = vec4( outgoingLight, diffuseColor.a );
+					#include <tonemapping_fragment>
+					#include <encodings_fragment>
+					#include <fog_fragment>
+					#include <premultiplied_alpha_fragment>
+					#include <dithering_fragment>
+
+					// TODO: output normal, roughness, metalness
+				}
+			`
+
+		} );
+
 	}
 
 } );
@@ -127,4 +261,6 @@ THREE.SSRRPass.prototype = Object.assign( Object.create( THREE.Pass.prototype ),
 THREE.SSRRPass.DEFAULT = 0;
 THREE.SSRRPass.NORMAL = 1;
 THREE.SSRRPass.DEPTH = 2;
-THREE.SSRRPass.REFLECTION = 3;
+THREE.SSRRPass.ROUGHNESS = 3;
+THREE.SSRRPass.METALNESS = 4;
+THREE.SSRRPass.REFLECTION = 5;
