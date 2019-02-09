ef20c56fb3188513aec0519419617fab8fda57ca
Garrett Johnson <garrett.kjohnson@gmail.com>
2019-02-09 15:42:40 -0800

    Get basic approach working

--- volume-lights/Blit.js
+++ volume-lights/Blit.js
@@ -1,4 +1,4 @@
-window.Blit = ( function() {
+window.blit = ( function() {
 
     const scene = new THREE.Scene();
     const camera = new THREE.OrthographicCamera( -0.5, 0.5, 0.5, -0.5, -0.5, 0.5 );
@@ -22,30 +22,32 @@ window.getBlendMaterial = function() {
         uniforms: {
             texture1: { value: null },
             texture2: { value: null },
-            alpha: { value: 0 },
+            iterations: { value: 1 }
         },
 
         vertexShader: `
             varying vec2 vUv;
             void main()	{
                 vUv = uv;
-                gl_Position = vec4( position, 1.0 );
+                gl_Position = vec4( position * 2.0, 1.0 );
             }
         `,
 
         fragmentShader: `
+            highp float;
+            highp int;
             varying vec2 vUv;
             uniform sampler2D texture1;
             uniform sampler2D texture2;
-            uniform float alpha;
+            uniform int iterations;
 
             void main() {
 
                 vec4 s1 = texture2D( texture1, vUv );
                 vec4 s2 = texture2D( texture2, vUv );
 
-                gl_FragColor = mix( s1, s2, alpha );
-                gl_FragColor.a = 1;
+                gl_FragColor = mix( s1, s2, 1.0 / float(iterations) );
+                gl_FragColor.a = 1.0;
             }
         `
 
