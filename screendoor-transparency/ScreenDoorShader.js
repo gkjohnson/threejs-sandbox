ae9c7b85fdcc236c105e859497a0671fa1ec56fc
Garrett Johnson <garrett.kjohnson@gmail.com>
2019-03-15 16:40:57 -0700

    Use a function to generate the dither pattern

--- screendoor-transparency/ScreenDoorShader.js
+++ screendoor-transparency/ScreenDoorShader.js
@@ -1,28 +1,28 @@
 function createDitherTexture() {
 
-	const data = new Float32Array(16);
-	data[0] = 1.0 / 17.0;
-	data[1] = 9.0 / 17.0;
-	data[2] = 3.0 / 17.0;
-	data[3] = 11.0 / 17.0;
+	const data = new Float32Array( 16 );
+	data[ 0 ] = 0.0 / 16.0;
+	data[ 1 ] = 8.0 / 16.0;
+	data[ 2 ] = 2.0 / 16.0;
+	data[ 3 ] = 10.0 / 16.0;
 
-	data[4] = 13.0 / 17.0;
-	data[5] = 5.0 / 17.0;
-	data[6] = 15.0 / 17.0;
-	data[7] = 7.0 / 17.0;
+	data[ 4 ] = 12.0 / 16.0;
+	data[ 5 ] = 4.0 / 16.0;
+	data[ 6 ] = 14.0 / 16.0;
+	data[ 7 ] = 6.0 / 16.0;
 
-	data[8] = 4.0 / 17.0;
-	data[9] = 12.0 / 17.0;
-	data[10] = 2.0 / 17.0;
-	data[11] = 10.0 / 17.0;
+	data[ 8 ] = 3.0 / 16.0;
+	data[ 9 ] = 11.0 / 16.0;
+	data[ 10 ] = 1.0 / 16.0;
+	data[ 11 ] = 9.0 / 16.0;
 
-	data[12] = 16.0 / 17.0;
-	data[13] = 8.0 / 17.0;
-	data[14] = 14.0 / 17.0;
-	data[15] = 6.0 / 17.0;
+	data[ 12 ] = 15.0 / 16.0;
+	data[ 13 ] = 9.0 / 16.0;
+	data[ 14 ] = 13.0 / 16.0;
+	data[ 15 ] = 5.0 / 16.0;
 
 
-	ditherTex = new THREE.DataTexture(data, 4, 4, THREE.LuminanceFormat, THREE.FloatType);
+	ditherTex = new THREE.DataTexture( data, 4, 4, THREE.LuminanceFormat, THREE.FloatType );
 	ditherTex.minFilter = THREE.NearestFilter;
 	ditherTex.magFilter = THREE.NearestFilter;
 	ditherTex.anisotropy = 1;
@@ -32,22 +32,23 @@ function createDitherTexture() {
 	ditherTex.needsUpdate = true;
 
 	return ditherTex;
+
 }
 
-function cloneShader(shader, uniforms, defines) {
+function cloneShader( shader, uniforms, defines ) {
 
-	const newShader = Object.assign({}, shader);
-	newShader.uniforms = THREE.UniformsUtils.merge([
+	const newShader = Object.assign( {}, shader );
+	newShader.uniforms = THREE.UniformsUtils.merge( [
 		newShader.uniforms,
 		uniforms
-	]);
-	newShader.defines = Object.assign({}, defines);
+	] );
+	newShader.defines = Object.assign( {}, defines );
 
 	return newShader;
 
 }
 
-function DitheredTransparencyShaderMixin(shader) {
+function DitheredTransparencyShaderMixin( shader ) {
 
 	const defineKeyword = 'ENABLE_DITHER_TRANSPARENCY';
  	const newShader = cloneShader(
@@ -56,22 +57,36 @@ function DitheredTransparencyShaderMixin(shader) {
  			ditherTex: { value: null },
  		},
  		{
- 			[defineKeyword]: 1,
+ 			[ defineKeyword ]: 1,
  		}
 	);
 
 	newShader.fragmentShader = `
+		 	// adapted from https://www.shadertoy.com/view/Mlt3z8
+			float bayerDither2x2( vec2 v ) {
+				return mod( 3.0 * v.y + 2.0 * v.x, 4.0 );
+			}
+
+			float bayerDither4x4( vec2 v ) {
+				vec2 P1 = mod( v, 2.0 );
+				vec2 P2 = mod( floor( 0.5  * v ), 2.0 );
+				return 4.0 * bayerDither2x2( P1 ) + bayerDither2x2( P2 );
+			}
+
+
 			uniform sampler2D ditherTex;
 			${newShader.fragmentShader}
-		`.replace(
-			/main\(\) {/,
-			v => `
+	`.replace(
+		/main\(\) {/,
+		v => `
 			${v}
 			#if ${defineKeyword}
-			if(texture2D(ditherTex, gl_FragCoord.xy / 4.0).r > opacity) discard;
+			//if( texture2D( ditherTex, gl_FragCoord.xy / 4.0 ).r > opacity ) discard;
+			if( ( bayerDither4x4( floor( mod( gl_FragCoord.xy, 4.0 ) ) ) ) / 16.0 >= opacity ) discard;
+
 			#endif
-			`,
-		);
+		`
+	);
 
 	return newShader;
 
