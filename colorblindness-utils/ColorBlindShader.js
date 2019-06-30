d1bfb57880b9414e5a422f88f9f08ed217b5cbed
Garrett Johnson <garrett.kjohnson@gmail.com>
2019-06-30 07:21:45 -0700

    Update colorblind shader

--- colorblindness-utils/ColorBlindShader.js
+++ colorblindness-utils/ColorBlindShader.js
@@ -1,3 +1,6 @@
+// References
+// http://web.archive.org/web/20081014161121/http://www.colorjack.com/labs/colormatrix/
+// http://mapeper.github.io/jsColorblindSimulator/
 var ColorBlindShader = {
 
 	uniforms: {
@@ -19,52 +22,37 @@ var ColorBlindShader = {
 	`,
 
 	fragmentShader: `
-		// TODO: Verify the results here -- whay is tritanope not teal / red?
-		uniform sampler2D tDiffuse;
 		varying vec2 vUv;
 
 		void main() {
 
 			vec3 rgb = texture2D( tDiffuse, vUv ).rgb;
 
-			mat3 rgb2lms;
-			rgb2lms[0] = vec3(17.8824, 3.45565, 0.0299566);
-			rgb2lms[1] = vec3(43.5161, 27.1554, 0.184309);
-			rgb2lms[2] = vec3(4.11935, 3.86714, 1.46709);
-
-			mat3 lms2rgb;
-			lms2rgb[0] = vec3(0.0809444479, -0.0102485335, -0.000365296938);
-			lms2rgb[1] = vec3(-0.130504409, 0.0540193266, -0.00412161469);
-			lms2rgb[2] = vec3(0.116721066, -0.113614708, 0.693511405);
-
 			mat3 dMat;
-			dMat[0] = vec3(1.0, 0.494207, 0.0);
-			dMat[1] = vec3(0.0, 0.0, 0.0);
-			dMat[2] = vec3(0.0, 1.24827	, 1.0);
+			dMat[0] = vec3(0.625, 0.375, 0.0);
+			dMat[1] = vec3(0.7, 0.3, 0.0);
+			dMat[2] = vec3(0.0, 0.3, 0.7);
 
 			mat3 pMat;
-			pMat[0] = vec3(0.0, 0.0, 0.0);
-			pMat[1] = vec3(2.02344, 1.0, 0.0);
-			pMat[2] = vec3(-2.52581, 0.0, 1.0);
+			pMat[0] = vec3(0.56667, 0.43333, 0.0);
+			pMat[1] = vec3(0.55833, 0.44167, 0.0);
+			pMat[2] = vec3(0.0, 0.24167, 0.75833);
 
 			mat3 tMat;
-			tMat[0] = vec3(1.0, 0.0, -0.395913);
-			tMat[1] = vec3(0.0, 1.0, 0.801109);
-			tMat[2] = vec3(0.0, 0.0, 0.0);
+			tMat[0] = vec3(0.95, 0.05, 0.0);
+			tMat[1] = vec3(0.0, 0.43333, 0.56667);
+			tMat[2] = vec3(0.0, 0.475, 0.525);
 
-			vec3 lms = rgb2lms * rgb;
+			vec3 res = rgb;
 
-			#if (MODE == 0)
-			vec3 cb = lms;
-			#elif (MODE == 1)
-			vec3 cb = dMat * lms;
+			#if (MODE == 1)
+			res = dMat * rgb;
 			#elif (MODE == 2)
-			vec3 cb = pMat * lms;
+			res = pMat * rgb;
 			#elif (MODE == 3)
-			vec3 cb = tMat * lms;
+			res = tMat * rgb;
 			#endif
 
-			vec3 res = lms2rgb * cb;
 			gl_FragColor = vec4(res, 1.0);
 		}
 
