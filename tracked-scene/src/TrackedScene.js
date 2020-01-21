e6f196e4eb503c70fbf75486fcdbba6343824e17
Garrett Johnson <garrett.kjohnson@gmail.com>
2020-01-21 08:11:10 -0800

    Update TrackedScene.js

--- tracked-scene/src/TrackedScene.js
+++ tracked-scene/src/TrackedScene.js
@@ -1,12 +1,39 @@
+let addChildCb, removeChildCb;
+
+addChildCb = function(e) {
+	const child = e.child;
+	child.traverse(c => {
+		c.addEventListener('childadded', addChildCb);
+		c.addEventListener('childremoved', removeChildCb);
+	});
+
+	this.dispatchEvent({ type: 'addedtoscene', child: child });
+	child.traverse(c => {
+		this.dispatchEvent({ type: 'addedtoscene', child: c });
+	});
+}.bind(this);
+
+removeChildCb = function(e) {
+	const child = e.child;
+	child.traverse(c => {
+		c.removeEventListener('childadded', addChildCb);
+		c.removeEventListener('childremoved', removeChildCb);
+	});
+
+	this.dispatchEvent({ type: 'removedfromscene', child: child });
+	child.traverse(c => {
+		this.dispatchEvent({ type: 'removedfromscene', child: c });
+	});
+}.bind(this);
+
 class TrackedScene extends THREE.Scene {
 
   constructor( ...args ) {
   
     super( ...args );
-    this.allObjects = new Set();
-    
-    // ...
-  
+		this.addEventListener('childadded', addChildCb);
+		this.addEventListener('childremoved', removeChildCb);
+		
   }
 
 }
