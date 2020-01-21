96e67e8092d271fb593c1ad376d80b622ca205f7
Garrett Johnson <garrett.kjohnson@gmail.com>
2020-01-21 08:05:00 -0800

    Create addRemoveOverrides.js

---
+++ tracked-scene/src/addRemoveOverrides.js
@@ -0,0 +1,21 @@
+import { Object3D } from 'three';
+
+const _ogAdd = Object3D.prototype.add;
+const _ogRemove = Object3D.prototype.remove;
+function add(...args) {
+    _ogAdd.apply(this, args);
+
+    if (args.length === 1) {
+        this.dispatchEvent({ type: 'childadded', child: args[0] });
+    }
+};
+
+function remove(...args) {
+    _ogRemove.apply(this, args);
+
+    if (args.length === 1) {
+        this.dispatchEvent({ type: 'childremoved', child: args[0] });
+    }
+};
+
+export { add, remove }
