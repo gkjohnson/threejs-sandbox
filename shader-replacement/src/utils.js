62ff9533739997572333d424317e608852d12d85
Garrett Johnson <garrett.kjohnson@gmail.com>
2020-03-05 11:19:47 -0800

    Create utils.js

---
+++ shader-replacement/src/utils.js
@@ -0,0 +1,16 @@
+function gatherMeshes( scene, target ) {
+
+	target.length = 0;
+	scene.traverse( c => { 
+	
+		if ( c.isMesh || c.isSkinnedMesh ) {
+
+			target.push( c );
+
+		}
+	
+	} );
+
+}
+
+export { gatherMeshes };
