73d49fa27ae49d08c6a3275367168bb84f13a81f
Garrett Johnson <garrett.kjohnson@gmail.com>
2020-03-06 19:26:08 -0800

    Whitespace updates

--- shader-replacement/src/ShaderReplacement.js
+++ shader-replacement/src/ShaderReplacement.js
@@ -1,3 +1,5 @@
+import { ShaderMaterial } from '//unpkg.com/three@0.114.0/build/three.module.js';
+
 export class ShaderReplacement {
 
 	constructor( shader ) {
@@ -8,8 +10,9 @@ export class ShaderReplacement {
 
 	}
 
-	replace( scene, recursive = false, cacheCurrentMaterial = false ) {
+	replace( scene, recursive = false, cacheCurrentMaterial = true ) {
 
+		const scope = this;
 		function applyMaterial( object ) {
 
 			if ( ! object.isMesh && ! object.skinnedMesh ) {
@@ -20,7 +23,7 @@ export class ShaderReplacement {
 
 			if ( ! replacementMaterials.has( object ) ) {
 
-				const replacementMaterial = this.createMaterial( object );
+				const replacementMaterial = scope.createMaterial( object );
 				replacementMaterials.set( object, replacementMaterial );
 
 			}
@@ -42,46 +45,47 @@ export class ShaderReplacement {
 				originalMaterial = originalMaterials.get( object );
 
 			}
-			this.updateUniforms( object, originalMaterial, replacementMaterial );
+
+			scope.updateUniforms( object, originalMaterial, replacementMaterial );
 
 			object.material = replacementMaterial;
 
 		}
-		
+
 		const replacementMaterials = this._replacementMaterials;
 		const originalMaterials = this._originalMaterials;
 		if ( Array.isArray( scene ) ) {
-			
+
 			if ( recursive ) {
-			
+
 				for ( let i = 0, l = scene.length; i < l; i ++ ) {
-				
+
 					scene[ i ].traverse( applyMaterial );
-					
+
 				}
 
 			} else {
-				
+
 				for ( let i = 0, l = scene.length; i < l; i ++ ) {
-				
+
 					applyMaterial( scene[ i ] );
-					
+
 				}
-				
+
 			}
-			
+
 		} else {
-		
+
 			if ( recursive ) {
-			
+
 				scene.traverse( applyMaterial );
-				
+
 			} else {
-			
+
 				applyMaterial( scene );
-				
+
 			}
-			
+
 		}
 
 	}
@@ -89,49 +93,49 @@ export class ShaderReplacement {
 	reset( scene, recursive ) {
 
 		function resetMaterial( object ) {
-			
+
 			if ( originalMaterials.has( object ) ) {
 
 				object.material = originalMaterials.get( object );
 
 			}
-			
+
 		}
 
 		const currentScene = this._currentScene;
 		const originalMaterials = this._originalMaterials;
 		if ( Array.isArray( scene ) ) {
-		
-			if ( recursive ) { 
-				
-				for ( let i = 0, l = scene.length; i < l; i ++ ) {
-				
-					resetMaterial( scene[ i ] );
-					
-				}
-				
-			} else {
-				
-				for ( let i = 0, l = scene.length; i < l; i ++ ) {
-				
-					scene[ i ].traverse( resetMaterial );
-					
-				}
-				
-			}
-			
-		} else {
-			
+
 			if ( recursive ) {
-				
+
+				for ( let i = 0, l = scene.length; i < l; i ++ ) {
+
+					resetMaterial( scene[ i ] );
+
+				}
+
+			} else {
+
+				for ( let i = 0, l = scene.length; i < l; i ++ ) {
+
+					scene[ i ].traverse( resetMaterial );
+
+				}
+
+			}
+
+		} else {
+
+			if ( recursive ) {
+
 				scene.traverse( resetMaterial );
 
 			} else {
-				
+
 				resetMaterial( scene );
-				
+
 			}
-			
+
 		}
 
 	}
@@ -150,86 +154,86 @@ export class ShaderReplacement {
 		const materialDefines = material.defines;
 		const targetDefines = target.defines;
 		for ( const key in materialDefines ) {
-		
+
 			if ( materialDefines[ key ] !== targetDefines[ key ] ) {
-			
+
 				targetDefines[ key ] = materialDefines[ key ];
 				target.needsUpdate = true;
-				
+
 			}
-			
+
 		}
-		
+
 		for ( const key in targetDefines ) {
 
 			if ( ! key in materialDefines ) {
 
 				if ( key in originalDefines ) {
-					
+
 					if ( originalDefines[ key ] !== targetDefines[ key ] ) {
-						
+
 						targetDefines[ key ] = originalDefines[ key ];
 						target.needsUpdate = true;
-						
+
 					}
-					
+
 				} else {
-				
+
 					delete targetDefines[ key ];
 					target.needsUpdate = true;
-					
+
 				}
-			    
+
 		    	} else if ( materialDefines[ key ] !== targetDefines[ key ] ) {
-			
+
 				targetDefines[ key ] = materialDefines[ key ];
 				target.needsUpdate = true;
-				
+
 			}
-			
+
 		}
-		
+
 		// NOTE: we shouldn't have to worry about using copy / equals on colors, vectors, or arrays here
 		// because we promise not to change the values.
 		const targetUniforms = target.uniforms;
 		if ( material.isShaderMaterial ) {
-			
-			const materialUniforms = material.uniformns;
+
+			const materialUniforms = material.uniforms;
 			for ( const key in targetUniforms ) {
-				
+
 				const materialUniform = materialUniforms[ key ];
 				const targetUniform = targetUniforms[ key ];
 				if ( materialUniform && materialUniform.value !== targetUniform.value ) {
-					
+
 					targetUniform.value = materialUniform.value;
 					if ( targetUniform.value.isTexture ) {
-					
+
 						target.needsUpdate = true;
-						
+
 					}
-					
+
 				}
-				
+
 			}
-			
+
 		} else {
-			
+
 			for ( const key in targetUniforms ) {
-				
+
 				const targetUniform = targetUniforms[ key ];
 				if ( key in material && material[ key ] !== targetUniform.value ) {
-					
+
 					targetUniform.value = material[ key ];
 					if ( targetUniform.value.isTexture ) {
-					
+
 						target.needsUpdate = true;
-						
+
 					}
-					
+
 				}
-				
+
 			}
-			
+
 		}
 
 	}
