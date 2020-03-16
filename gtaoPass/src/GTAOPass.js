672fb25252fa3553062f118f4eb2f7c9b9a56374
Garrett Johnson <garrett.kjohnson@gmail.com>
2020-03-15 18:59:50 -0700

    Add GTAO pass

---
+++ gtaoPass/src/GTAOPass.js
@@ -0,0 +1,270 @@
+import { Color, ShaderMaterial, WebGLRenderTarget, NearestFilter, RGBAFormat, FloatType } from '//unpkg.com/three@0.114.0/build/three.module.js';
+import { Pass } from '//unpkg.com/three@0.114.0/examples/jsm/postprocessing/Pass.js';
+import { CopyShader } from '//unpkg.com/three@0.114.0/examples/jsm/shaders/CopyShader.js';
+import { ShaderReplacement } from '../../shader-replacement/src/ShaderReplacement.js';
+import { PackedMipMapGenerator } from '../../custom-mipmap-generation/src/PackedMipMapGenerator.js';
+import { LinearDepthShader } from '../../screenSpaceReflectionsPass/src/LinearDepthShader.js';
+import { PackedShader } from '../../screenSpaceReflectionsPass/src/PackedShader.js';
+import { LinearDepthDisplayShader, LinearMipDepthDisplayShader } from './DebugShaders.js';
+import { PackedNormalDisplayShader } from '../../screenSpaceReflectionsPass/src/DebugShaders.js';
+import { GTAOShader } from './GTAOShader.js';
+
+const _gtaoMaterial = new ShaderMaterial( GTAOShader );
+const _gtaoQuad = new Pass.FullScreenQuad( _gtaoMaterial );
+
+const _debugPackedMaterial = new ShaderMaterial( PackedNormalDisplayShader );
+const _debugPackedQuad = new Pass.FullScreenQuad( _debugPackedMaterial );
+
+const _copyMaterial = new ShaderMaterial( CopyShader );
+const _copyQuad = new Pass.FullScreenQuad( _copyMaterial );
+
+const _debugDepthMaterial = new ShaderMaterial( LinearDepthDisplayShader );
+const _debugDepthQuad = new Pass.FullScreenQuad( _debugDepthMaterial );
+
+const _debugMipDepthMaterial = new ShaderMaterial( LinearMipDepthDisplayShader );
+const _debugMipDepthQuad = new Pass.FullScreenQuad( _debugMipDepthMaterial );
+
+const _blackColor = new Color( 0 );
+const offsets = [ 0.0, 0.5, 0.25, 0.75 ];
+const rotations = [ 60.0, 300.0, 180.0, 240.0, 120.0, 0.0 ];
+
+export class GTAOPass extends Pass {
+
+	constructor( scene, camera, options = {} ) {
+
+		super();
+
+		this.enabled = true;
+		this.needsSwap = true;
+
+		this.scene = scene;
+		this.camera = camera;
+		this.debug = {
+			display: GTAOPass.DEFAULT,
+			depthLevel: - 1,
+		};
+		this.drawIndex = 0;
+
+		this._gtaoBuffer =
+			new WebGLRenderTarget( 256, 256, {
+				minFilter: NearestFilter,
+				magFilter: NearestFilter,
+				format: RGBAFormat,
+				type: FloatType
+			} );
+
+		this._depthBuffer =
+			new WebGLRenderTarget( 256, 256, {
+				minFilter: NearestFilter,
+				magFilter: NearestFilter,
+				format: RGBAFormat,
+				type: FloatType
+			} );
+		this._depthPyramidBuffer =
+			new WebGLRenderTarget( 256, 256, {
+				minFilter: NearestFilter,
+				magFilter: NearestFilter,
+				format: RGBAFormat,
+				type: FloatType
+			} );
+		this._depthReplacement = new ShaderReplacement( LinearDepthShader );
+		this._depthPyramidGenerator = new PackedMipMapGenerator(
+			`
+			float depth = 0.0;
+
+			#pragma unroll_loop
+			for ( int i = 0; i < SAMPLES; i ++ ) {
+
+				float sample = samples[ i ].r;
+				if ( sample != 0.0 ) {
+
+					depth =
+						depth == 0.0 ?
+							sample :
+							max( sample, depth );
+
+				}
+
+			}
+
+			gl_FragColor = vec4( depth );
+
+			`
+		);
+
+
+		this._normalBuffer =
+			new WebGLRenderTarget( 256, 256, {
+				minFilter: NearestFilter,
+				magFilter: NearestFilter,
+				format: RGBAFormat,
+				type: FloatType
+			} );
+		this._normalReplacement = new ShaderReplacement( PackedShader );
+
+	}
+
+	dispose() {
+
+	}
+
+	setSize( width, height ) {
+
+		const renderWidth = width;
+		const renderHeight = height;
+
+		this._depthBuffer.setSize( renderWidth, renderHeight );
+		this._normalBuffer.setSize( renderWidth, renderHeight );
+		this._gtaoBuffer.setSize( renderWidth, renderHeight );
+
+	}
+
+	render( renderer, writeBuffer, readBuffer, delta, maskActive ) {
+
+		const drawIndex = this.drawIndex;
+		this.drawIndex = ( drawIndex + 1 ) % 6;
+
+		const scene = this.scene;
+		const camera = this.camera;
+		const debug = this.debug;
+		const finalBuffer = this.renderToScreen ? null : writeBuffer;
+
+		const _prevClearColor = new Color();
+
+		// Save the previous scene state
+		const prevClearAlpha = renderer.getClearAlpha();
+		const prevAutoClear = renderer.autoClear;
+		const prevOverride = scene.overrideMaterial;
+		const prevAutoUpdate = scene.autoUpdate;
+		const prevRenderTarget = renderer.getRenderTarget();
+		const pevShadowEnabled = renderer.shadowMap.enabled;
+		const prevSceneBackground = scene.background;
+		_prevClearColor.copy( renderer.getClearColor() );
+
+		const replaceOriginalValues = () => {
+
+			// Restore renderer settings
+			scene.overrideMaterial = prevOverride;
+			renderer.setRenderTarget( prevRenderTarget );
+			renderer.setClearColor( this._prevClearColor, prevClearAlpha );
+			renderer.autoClear = prevAutoClear;
+			renderer.shadowMap.enabled = pevShadowEnabled;
+			scene.autoUpdate = prevAutoUpdate;
+			scene.background = prevSceneBackground;
+			depthReplacement.reset( scene, true );
+
+		};
+
+		// draw depth pyramid
+		const depthPyramidGenerator = this._depthPyramidGenerator;
+		const depthReplacement = this._depthReplacement;
+		const depthBuffer = this._depthBuffer;
+		const depthPyramidBuffer = this._depthPyramidBuffer;
+		depthReplacement.replace( scene, true, true );
+		renderer.setRenderTarget( depthBuffer );
+		renderer.setClearColor( _blackColor, 0.0 );
+		renderer.clear();
+		renderer.render( scene, camera );
+
+		depthPyramidGenerator.update( depthBuffer, depthPyramidBuffer, renderer, false );
+
+		if ( debug.display === GTAOPass.DEPTH_PYRAMID ) {
+
+			renderer.setRenderTarget( finalBuffer );
+
+			const level = debug.depthLevel;
+			if ( level < 0 ) {
+
+				_debugDepthMaterial.uniforms.texture.value = depthPyramidBuffer.texture;
+				_debugDepthMaterial.uniforms.divide.value = camera.far;
+				_debugDepthQuad.render( renderer );
+
+			} else {
+
+				_debugMipDepthMaterial.uniforms.originalSize.value.set(
+					Math.floor( depthBuffer.texture.image.width ),
+					Math.floor( depthBuffer.texture.image.height )
+				);
+				_debugMipDepthMaterial.uniforms.level.value = level;
+				_debugMipDepthMaterial.uniforms.texture.value = depthPyramidBuffer.texture;
+				_debugMipDepthMaterial.uniforms.divide.value = camera.far;
+				_debugMipDepthQuad.render( renderer );
+
+			}
+
+			// copy the pyramid at the target level to the screen
+			replaceOriginalValues();
+			return;
+
+		}
+
+		// Roughness / Normal pass
+		const packedReplacement = this._normalReplacement;
+		const packedBuffer = this._normalBuffer;
+		packedReplacement.replace( scene, true, false );
+		renderer.setRenderTarget( packedBuffer );
+		renderer.clear();
+		renderer.render( scene, camera );
+		if ( debug.display === GTAOPass.NORMAL ) {
+
+			renderer.setRenderTarget( finalBuffer );
+			renderer.clear();
+
+			_debugPackedMaterial.uniforms.displayRoughness.value = 0.0;
+			_debugPackedMaterial.uniforms.texture.value = packedBuffer.texture;
+			_debugPackedQuad.render( renderer );
+			replaceOriginalValues();
+			return;
+
+		}
+
+		// Run the GTAO sampling
+		const gtaoBuffer = this._gtaoBuffer;
+		const width = Math.floor( gtaoBuffer.texture.image.width );
+		const height = Math.floor( gtaoBuffer.texture.image.height );
+		const gtaoMaterial = _gtaoMaterial;
+		const gtaoQuad = _gtaoQuad;
+		const projection = camera.projectionMatrix;
+		gtaoMaterial.uniforms.params.value.set(
+			rotations[ 0 % 6 ] / 360.0,
+			offsets[ ( 0 / 6 ) % 4 ]
+		);
+		gtaoMaterial.uniforms.projInfo.value.set(
+			2.0 / ( width * projection.elements[ 4 * 1 + 1 ] ),
+			2.0 / ( height * projection.elements[ 4 * 2 + 2 ] ),
+			- 1.0 / ( projection.elements[ 4 * 1 + 1 ] ),
+			- 1.0 / ( width * projection.elements[ 4 * 2 + 2 ] )
+		);
+		gtaoMaterial.uniforms.clipInfo.value.set(
+			camera.near,
+			camera.far,
+			0.5 * ( height / ( 2.0 * Math.tan( camera.fov * 0.5 ) ) )
+		);
+		gtaoMaterial.uniforms.normalBuffer.value = packedBuffer.texture;
+		gtaoMaterial.uniforms.depthPyramid.value = depthPyramidBuffer.texture;
+		gtaoMaterial.uniforms.depthPyramidSize.value.set(
+			Math.floor( depthBuffer.texture.image.width ),
+			Math.floor( depthBuffer.texture.image.height )
+		);
+
+		renderer.setRenderTarget( finalBuffer );
+		renderer.clear();
+		gtaoQuad.render( renderer );
+
+		// TODO spatial denoise via blur
+
+		// TODO temporal reproject denoise and accumulate
+
+		// renderer.setRenderTarget( finalBuffer );
+		// _copyMaterial.uniforms.tDiffuse.value = readBuffer.texture;
+		// _copyQuad.render( renderer );
+
+		replaceOriginalValues();
+
+	}
+
+}
+
+GTAOPass.DEFAULT = 0;
+GTAOPass.DEPTH_PYRAMID = 1;
+GTAOPass.NORMAL = 2;
---
+++ gtaoPass/src/GTAOShader.js
@@ -0,0 +1,208 @@
+import { Vector2, Vector4 } from '//unpkg.com/three@0.114.0/build/three.module.js';
+import { sampleFunctions } from '../../custom-mipmap-generation/src/mipSampleFunctions.js';
+
+export const GTAOShader = {
+
+	uniforms: {
+
+		normalBuffer: { value: null },
+		depthPyramid: { value: null },
+		depthPyramidSize: { value: new Vector2() },
+
+		clipInfo: { value: new Vector4 },
+		projInfo: { value: new Vector4() },
+		params: { value: new Vector2() },
+
+	},
+
+	vertexShader:
+		/* glsl */`
+		varying vec2 vUv;
+		void main() {
+
+			vUv = uv;
+			gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+
+		}
+	`,
+
+	fragmentShader:
+		/* glsl */`
+		#define PREFETCH_CACHE_SIZE 8
+		#define NUM_MIP_LEVELS 5
+		#define NUM_STEPS		8
+		#define RADIUS			2.0		// in world space
+
+		// #define PI				3.1415926535897932
+		#define TWO_PI			6.2831853071795864
+		#define HALF_PI			1.5707963267948966
+		#define ONE_OVER_PI		0.3183098861837906
+
+		#include <common>
+		#include <packing>
+		varying vec2 vUv;
+
+		uniform sampler2D normalBuffer;
+		uniform sampler2D depthPyramid;
+		uniform vec2 depthPyramidSize;
+
+		uniform vec4 clipInfo;
+		uniform vec4 projInfo;
+		uniform vec2 params;
+
+		${ sampleFunctions }
+
+		float round( float f ) {
+
+			return f < 0.5 ? floor( f ) : ceil( f );
+
+		}
+
+		vec2 round( vec2 v ) {
+
+			v.x = round( v.x );
+			v.y = round( v.y );
+			return v;
+
+		}
+
+		vec4 GetViewPosition( vec2 uv, float currStep ) {
+
+			int miplevel = int(
+				clamp(
+					floor(
+						log2(
+							currStep / float( PREFETCH_CACHE_SIZE )
+						)
+					),
+					0.0,
+					float( NUM_MIP_LEVELS - 1 )
+				)
+			);
+
+			vec2 mipcoord = uv / depthPyramidSize;
+			float d = packedTexture2DLOD( depthPyramid, mipcoord, miplevel, depthPyramidSize ).r;
+
+			// TODO: d is expected to be [ 0.0, 1.0 ]
+			vec4 ret = vec4( 0.0 );
+			ret.w = d;
+			ret.z = clipInfo.x + d * ( clipInfo.y - clipInfo.x );
+			ret.xy = ( uv * projInfo.xy + projInfo.zw ) * ret.z;
+
+			return ret;
+
+		}
+
+		float Falloff( float dist2, float cosh ) {
+			#define FALLOFF_START2	0.16
+			#define FALLOFF_END2	4.0
+
+			return 2.0 * clamp(
+				( dist2 - FALLOFF_START2 ) / ( FALLOFF_END2 - FALLOFF_START2 ),
+				0.0,
+				1.0
+			);
+
+		}
+
+		void main() {
+
+			vec2 uv = vUv;
+
+			vec2 loc = gl_FragCoord.xy;// depthPyramidSize * uv;
+			vec4 vpos = GetViewPosition( loc, 1.0 );
+
+			// if ( vpos.w == 1.0 ) {
+
+			// 	my_FragColor0 = 1.0;
+			// 	return;
+
+			// }
+
+			vec4 s;
+			vec3 vnorm	= texture2D( normalBuffer, vUv ).rgb;
+			vec3 vdir	= normalize( - vpos.xyz );
+			vec3 dir, ws;
+
+			// calculation uses left handed system
+			vnorm.z = - vnorm.z;
+
+			// TODO: use a noise function o texture here. Halton? Poisson?
+			vec2 noises	= vec2( 0.0 ); //texelFetch( noise, mod( loc, 4.0 ), 0.0 ).rg;
+			vec2 offset;
+			vec2 horizons = vec2( - 1.0, - 1.0 );
+
+			float radius = ( RADIUS * clipInfo.z ) / vpos.z;
+			radius = max( float( NUM_STEPS ), radius );
+
+			float stepsize	= radius / float( NUM_STEPS );
+			float phi		= ( params.x + noises.x ) * PI;
+			float ao		= 0.0;
+			float division	= noises.y * stepsize;
+			float currstep	= 1.0 + division + 0.25 * stepsize * params.y;
+			float dist2, invdist, falloff, cosh;
+
+			dir = vec3( cos( phi ), sin( phi ), 0.0 );
+			horizons = vec2( - 1.0 );
+
+			// calculate horizon angles
+			for ( int j = 0; j < NUM_STEPS; ++ j ) {
+
+				offset = round( dir.xy * currstep );
+
+				// h1
+				s = GetViewPosition( gl_FragCoord.xy + offset, currstep );
+				ws = s.xyz - vpos.xyz;
+
+				dist2 = dot( ws, ws );
+				invdist = inversesqrt( dist2 );
+				cosh = invdist * dot( ws, vdir );
+
+				falloff = Falloff( dist2, cosh );
+				horizons.x = max( horizons.x, cosh - falloff );
+
+				// h2
+				s = GetViewPosition( gl_FragCoord.xy - offset, currstep );
+				ws = s.xyz - vpos.xyz;
+
+				dist2 = dot( ws, ws );
+				invdist = inversesqrt( dist2 );
+				cosh = invdist * dot( ws, vdir );
+
+				falloff = Falloff( dist2, cosh );
+				horizons.y = max( horizons.y, cosh - falloff );
+
+				// increment
+				currstep += stepsize;
+
+			}
+
+			horizons = acos( horizons );
+
+			// calculate gamma
+			vec3 bitangent	= normalize( cross( dir, vdir ) );
+			vec3 tangent	= cross( vdir, bitangent );
+			vec3 nx			= vnorm - bitangent * dot( vnorm, bitangent );
+
+			float nnx		= length( nx );
+			float invnnx	= 1.0 / ( nnx + 1e-6 );			// to avoid division with zero
+			float cosxi		= dot( nx, tangent ) * invnnx;	// xi = gamma + HALF_PI
+			float gamma		= acos( cosxi ) - HALF_PI;
+			float cosgamma	= dot( nx, vdir ) * invnnx;
+			float singamma2	= -2.0 * cosxi;					// cos(x + HALF_PI) = -sin(x)
+
+			// clamp to normal hemisphere
+			horizons.x = gamma + max( - horizons.x - gamma, - HALF_PI );
+			horizons.y = gamma + min( horizons.y - gamma, HALF_PI );
+
+			// Riemann integral is additive
+			ao += nnx * 0.25 * (
+				( horizons.x * singamma2 + cosgamma - cos( 2.0 * horizons.x - gamma ) ) +
+				( horizons.y * singamma2 + cosgamma - cos( 2.0 * horizons.y - gamma ) ) );
+
+			gl_FragColor = vec4( ao );
+
+		}
+	`
+
+}
