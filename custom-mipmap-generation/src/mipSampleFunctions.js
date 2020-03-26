8d196a3646599c7c340d287d537d756a5bb5f952
Garrett Johnson <garrett.kjohnson@gmail.com>
2020-03-26 00:03:35 -0700

    unindent, variable rename

--- custom-mipmap-generation/src/mipSampleFunctions.js
+++ custom-mipmap-generation/src/mipSampleFunctions.js
@@ -36,33 +36,34 @@ export const sampleFunctions = /* glsl */`
 	// With original size argument
 	vec4 packedTexture2DLOD( sampler2D texture, vec2 uv, int level, vec2 originalPixelSize ) {
 
+		float floatLevel = float( level );
 		vec2 atlasSize;
-			atlasSize.x = floor( originalPixelSize.x * 1.5 );
-			atlasSize.y = originalPixelSize.y;
+		atlasSize.x = floor( originalPixelSize.x * 1.5 );
+		atlasSize.y = originalPixelSize.y;
 
-			// we stop making mip maps when one dimension == 1
-			float maxLevel = min( floor( log2( originalPixelSize.x ) ), floor( log2( originalPixelSize.y ) ) );
-			level = int( min( float( level ), maxLevel ) );
+		// we stop making mip maps when one dimension == 1
+		float maxLevel = min( floor( log2( originalPixelSize.x ) ), floor( log2( originalPixelSize.y ) ) );
+		floatLevel = min( floatLevel, maxLevel );
 
-			// use inverse pow of 2 to simulate right bit shift operator
-			vec2 currentPixelDimensions = floor( originalPixelSize / pow( 2.0, float( level ) ) );
-			vec2 pixelOffset = vec2(
-				level > 0 ? originalPixelSize.x : 0.0,
-				level > 0 ? currentPixelDimensions.y : 0.0
-			);
+		// use inverse pow of 2 to simulate right bit shift operator
+		vec2 currentPixelDimensions = floor( originalPixelSize / pow( 2.0, floatLevel ) );
+		vec2 pixelOffset = vec2(
+			floatLevel > 0.0 ? originalPixelSize.x : 0.0,
+			floatLevel > 0.0 ? currentPixelDimensions.y : 0.0
+		);
 
-			// "minPixel / atlasSize" samples the top left piece of the first pixel
-			// "maxPixel / atlasSize" samples the bottom right piece of the last pixel
-			vec2 minPixel = pixelOffset;
-			vec2 maxPixel = pixelOffset + currentPixelDimensions;
-			vec2 samplePoint = mix( minPixel, maxPixel, uv );
-			samplePoint /= atlasSize;
+		// "minPixel / atlasSize" samples the top left piece of the first pixel
+		// "maxPixel / atlasSize" samples the bottom right piece of the last pixel
+		vec2 minPixel = pixelOffset;
+		vec2 maxPixel = pixelOffset + currentPixelDimensions;
+		vec2 samplePoint = mix( minPixel, maxPixel, uv );
+		samplePoint /= atlasSize;
 
-			vec2 halfPixelSize = 1.0 / ( 2.0 * atlasSize );
-			samplePoint = min( samplePoint, maxPixel / atlasSize - halfPixelSize );
-			samplePoint = max( samplePoint, minPixel / atlasSize + halfPixelSize );
+		vec2 halfPixelSize = 1.0 / ( 2.0 * atlasSize );
+		samplePoint = min( samplePoint, maxPixel / atlasSize - halfPixelSize );
+		samplePoint = max( samplePoint, minPixel / atlasSize + halfPixelSize );
 
-			return texture2D( texture, samplePoint );
+		return texture2D( texture, samplePoint );
 
 	}
 
