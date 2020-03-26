583fcdfd0151d4a688d4b06570dd4e4a400cb3c9
Garrett Johnson <garrett.kjohnson@gmail.com>
2020-03-25 23:41:22 -0700

    clarify variable names, fix sampling

--- custom-mipmap-generation/src/mipSampleFunctions.js
+++ custom-mipmap-generation/src/mipSampleFunctions.js
@@ -5,13 +5,13 @@ export const sampleFunctions = /* glsl */`
 
 		// the fraction of the uv space used by the target mip
 		float targetSubview = 1.0 / pow( 2.0, float( level ) );
-		float texelWidth = 2.0 / 3.0;
-		vec2 scaledDimensions = vec2( targetSubview * texelWidth, targetSubview );
+		float widthRatio = 2.0 / 3.0;
+		vec2 scaledDimensions = vec2( targetSubview * widthRatio, targetSubview );
 
 		// all levels > 0 are on the right third of the texture
 		// y is offset from the bottom
 		vec2 offset = vec2(
-			level > 0 ? texelWidth : 0.0,
+			level > 0 ? widthRatio : 0.0,
 			level > 0 ? targetSubview : 0.0
 		);
 
@@ -34,36 +34,44 @@ export const sampleFunctions = /* glsl */`
 	}
 
 	// With original size argument
-	vec4 packedTexture2DLOD( sampler2D texture, vec2 uv, int level, vec2 originalSize ) {
+	vec4 packedTexture2DLOD( sampler2D texture, vec2 uv, int level, vec2 originalPixelSize ) {
 
-		float maxLevel = min( floor( log2( originalSize.x ) ), floor( log2( originalSize.y ) ) );
-		level = int( min( float( level ), maxLevel ) );
+		vec2 atlasSize;
+			atlasSize.x = floor( originalPixelSize.x * 1.5 );
+			atlasSize.y = originalPixelSize.y;
 
-		// use inverse pow of 2 to simulate right bit shift operator
-		vec2 currentPixelDimensions = floor( originalSize / pow( 2.0, float( level ) ) );
-		vec2 targetSubview = currentPixelDimensions / originalSize;
+			// we stop making mip maps when one dimension == 1
+			float maxLevel = min( floor( log2( originalPixelSize.x ) ), floor( log2( originalPixelSize.y ) ) );
+			level = int( min( float( level ), maxLevel ) );
 
-		float texelWidth = ( originalSize / floor( originalSize * 1.5 ) ).x;
+			// use inverse pow of 2 to simulate right bit shift operator
+			vec2 currentPixelDimensions = floor( originalPixelSize / pow( 2.0, float( level ) ) );
+			vec2 pixelOffset = vec2(
+				level > 0 ? originalPixelSize.x : 0.0,
+				level > 0 ? currentPixelDimensions.y : 0.0
+			);
 
-		vec2 scaledDimensions = vec2( texelWidth * targetSubview.x, targetSubview.y );
-		vec2 offset = vec2(
-			level > 0 ? texelWidth : 0.0,
-			level > 0 ? targetSubview.y : 0.0
-		);
+			vec2 minPixel = pixelOffset;
+			vec2 maxPixel = pixelOffset + currentPixelDimensions;
+			vec2 samplePoint = mix( minPixel, maxPixel, uv );
+			samplePoint /= atlasSize;
 
-		vec2 samplePoint = mix( offset, offset + scaledDimensions, uv );
-		return texture2D( texture, samplePoint );
+			vec2 halfPixelSize = 1.0 / ( 2.0 * atlasSize );
+			samplePoint = min( samplePoint, maxPixel / atlasSize - halfPixelSize );
+			samplePoint = max( samplePoint, minPixel / atlasSize + halfPixelSize );
+
+			return texture2D( texture, samplePoint );
 
 	}
 
-	vec4 packedTexture2DLOD( sampler2D texture, vec2 uv, float level, vec2 originalSize ) {
+	vec4 packedTexture2DLOD( sampler2D texture, vec2 uv, float level, vec2 originalPixelSize ) {
 
 		float ratio = mod( level, 1.0 );
 		int minLevel = int( floor( level ) );
 		int maxLevel = int( ceil( level ) );
 
-		vec4 minValue = packedTexture2DLOD( texture, uv, minLevel, originalSize );
-		vec4 maxValue = packedTexture2DLOD( texture, uv, maxLevel, originalSize );
+		vec4 minValue = packedTexture2DLOD( texture, uv, minLevel, originalPixelSize );
+		vec4 maxValue = packedTexture2DLOD( texture, uv, maxLevel, originalPixelSize );
 
 		return mix( minValue, maxValue, ratio );
 
