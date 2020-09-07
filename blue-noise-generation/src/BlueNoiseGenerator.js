e79fbd7e139b999e92fb179cae0a1e78f91f1c17
Garrett Johnson <garrett.kjohnson@gmail.com>
2020-09-06 23:58:30 -0700

    fixes

--- blue-noise-generation/src/BlueNoiseGenerator.js
+++ blue-noise-generation/src/BlueNoiseGenerator.js
@@ -7,18 +7,18 @@ export class BlueNoiseGenerator {
 
 		this.random = Math.random;
 		this.sigma = 1.5;
-		this.size = 256;
+		this.size = 64;
 		this.majorityPointsRatio = 0.1;
-		
+
 		this.samples = new BlueNoiseSamples( 1 );
-		this.savedSamples = BlueNoiseSamples( 1 );
+		this.savedSamples = new BlueNoiseSamples( 1 );
 
 	}
-	
+
 	generate() {
 
-		// http://cv.ulichney.com/papers/1993-void-cluster.pdf	
-				
+		// http://cv.ulichney.com/papers/1993-void-cluster.pdf
+
 		const {
 			samples,
 			savedSamples,
@@ -26,78 +26,85 @@ export class BlueNoiseGenerator {
 			majorityPointsRatio,
 			size,
 		} = this;
-		
+
 		samples.resize( size );
 		samples.setSigma( sigma );
 		savedSamples.resize( size );
 		savedSamples.setSigma( sigma );
-		
+
 		// 1. Random place the minority points.
 		const pointCount = Math.floor( size * size * majorityPointsRatio );
 		const initialSamples = samples.binaryPattern;
-		fillWithOnes( binaryPattern, pointCount );
+		fillWithOnes( initialSamples, pointCount );
 		shuffleArray( pointCount, this.random );
-		
-		for ( let i = 0, l = initialSamples.length; i ++ ) {
-			
-			if ( intialSamples[ i ] === 1 ) {
-				
+
+		for ( let i = 0, l = initialSamples.length; i < l; i ++ ) {
+
+			if ( initialSamples[ i ] === 1 ) {
+
 				samples.addPointIndex( i );
-				
+
 			}
 
 		}
-		
+
+
+		console.log(1);
+
 		// 2. Remove minority point that is in densest cluster and place it in a void.
 		while ( true ) {
-			
-			const minPoint = samples.find( 1, Math.max );
-			const majPoint = samples.find( 0, Math.min );
-			
-			if ( minPoint === majPoint ) {
-				
-				break;
-				
-			}
-			
+
+			const minPoint = samples.findIndex( 1, Math.max );
 			samples.removePointIndex( minPoint );
+
+			const majPoint = samples.findIndex( 0, Math.min );
+			if ( minPoint === majPoint ) {
+
+				break;
+
+			}
+
 			samples.addPointIndex( majPoint );
-			
+
 		}
-		
+
+		console.log(2);
+		return;
+
 		const ditherArray = new Uint32Array( size * size );
-		
+
 		// 3. PHASE I: Incrementally set the value of the dither array for each progressively
 		// less intensely clustered minority point to the number of remaining points down to 0.
 		// Remove the minority point after each iteration.
 		savedSamples.copy( samples );
-		
-		let rank = samples.count - 1;
+
+		let rank;
+		rank = samples.count - 1;
 		while ( rank > 0 ) {
-		
+
 			const minIndex = samples.findIndex( 1, Math.max );
 			samples.removePointIndex( minIndex );
-			
+
 			ditherArray[ minIndex ] = rank;
 			rank --;
-			
+
 		}
-		
+
 		// 4. PHASE II: Do the same thing for the largest voids up to half of the total pixels with
 		// the dither array value being set to the number of void points iterated over. Set the pixel
 		// to a minority point after each iteration. Track "rank" as remaining points. Use the initial
 		// binary pattern.
 		const totalSize = size * size;
-		let rank = savedSamples.count;
+		rank = savedSamples.count;
 		while ( rank < totalSize / 2 ) {
-			
+
 			const majIndex = savedSamples.findIndex( 0, Math.min );
 			savedSamples.addSamplePoint( majIndex );
 			ditherArray[ majIndex ] = rank;
 			rank ++;
-			
+
 		}
-		
+
 		// 5. PHASE III: Interpret majority points as minority points. Find the most intensely clustered
 		// minority point and insert 1. Increment rank for each point inserted and set the dither array
 		// value to "rank". Do this until rank reaches the max number of pixels in the grid.
@@ -105,14 +112,14 @@ export class BlueNoiseGenerator {
 		// Do we have to? Is there a faster way to do it?
 		savedSamples.invert();
 		while ( rank < totalSize ) {
-			
+
 			const majIndex = savedSamples.findIndex( 1, Math.max );
 			savedSamples.removeSamplePoint( majIndex );
 			ditherArray[ majIndex ] = rank;
 			rank ++;
-			
+
 		}
-		
+
 		return { data: ditherArray, maxValue: totalSize };
 
 	}
--- blue-noise-generation/src/BlueNoiseSamples.js
+++ blue-noise-generation/src/BlueNoiseSamples.js
@@ -1,7 +1,7 @@
 export class BlueNoiseSamples {
 
 	constructor( size ) {
-		
+
 		this.count = 0;
 		this.size = 1
 		this.sigma = 0;
@@ -9,152 +9,155 @@ export class BlueNoiseSamples {
 		this.lookupTable = null;
 		this.score = null;
 		this.binaryPattern = null;
-		
+
 		this.resize( size );
 		this.setSigma( 1.5 );
 
 	}
-	
+
 	findIndex( value, func ) {
-		
+
 		// TODO: note that finding the largest void may depend on the score radius not
 		// being arbitrarily cut off.
 		const { score, binaryPattern } = this;
 		let currValue = func( 0, 1 ) === 0 ? Infinity : - Infinity;
 		let currIndex = - 1;
 		for ( let i = 0, l = binaryPattern.length; i < l; i ++ ) {
-		
+
 			if ( binaryPattern[ i ] !== value ) continue;
-			
+
 			const pScore = score[ i ];
 			const winScore = func( pScore, currValue );
 			if ( winScore === pScore ) {
-				
+
 				currValue = pScore;
 				currIndex = i;
-				
+
 			}
-			
+
 		}
-		
+
 		return currIndex;
-		
+
 	}
-	
+
 	setSigma( sigma ) {
-	
+
 		if ( sigma === this.sigma ) {
-			
+
 			return;
-			
+
 		}
 
 		// generate a radius in which the score will be updated under the
 		// assumption that e^-10 is insignificant enough to be the border at
 		// which we drop off.
-		const radius = radius = ~ ~ ( Math.sqrt( 10 * 2 * ( sigma ** 2 ) ) + 1 );
-		const lookupWidth = 2 * this.radius + 1;
+		const radius = ~ ~ ( Math.sqrt( 10 * 2 * ( sigma ** 2 ) ) + 1 );
+		const lookupWidth = 2 * radius + 1;
 		const lookupTable = new Float32Array( lookupWidth * lookupWidth );
 		const sigma2 = sigma * sigma;
 		for ( let x = - radius; x <= radius; x ++ ) {
 
 			for ( let y = - radius; y <= radius; y ++ ) {
-			
-				const index = y * lookupWidth + x;
-				const dist = Math.sqrt( x * x + y * y );
-				const dist2 = dist * dist;
-				lookUpTable[ index ] = Math.E ** ( - dist2 / ( 2 * sigma2 ) )
-				
+
+				const index = ( radius + y ) * lookupWidth + x + radius;
+				const dist2 = x * x + y * y;
+				lookupTable[ index ] = Math.E ** ( - dist2 / ( 2 * sigma2 ) );
+
 			}
 
 		}
-			
+
 		this.lookupTable = lookupTable;
 		this.sigma = sigma;
 		this.radius = radius;
 
 	}
-	
+
 	resize( size ) {
-		
+
 		if ( this.size !== size ) {
-			
+
 			this.size = size;
 			this.score = new Float32Array( size * size );
 			this.binaryPattern = new Uint8Array( size * size );
 
 		}
-		
-		
+
+
 	}
-	
+
 	invert() {
-	
+
 		const { binaryPattern, score, size } = this;
-		
+
 		score.fill( 0 );
-		
+
 		for ( let i = 0, l = binaryPattern.length; i < l; i ++ ) {
-		
+
 			if ( binaryPattern[ i ] === 0 ) {
 
 				const y = ~ ~ ( i / size );
 				const x = i - y * size;
 				this.updateScore( x, y, 1 );
-			
+
 			} else {
-				
+
 				binaryPattern[ i ] = 0;
-				
+
 			}
-			
+
 		}
-		
+
 	}
-	
+
 	updateScore( x, y, multiplier ) {
-		
+
 		// TODO: dist can only be a few values so we should be able to make a lookup table
 		// and optimize out the sqrt and exponent operations.
 		// TODO: As we do this keep track of the highest and lowest scores for the majority
 		// and minority points because we'll want to use them soon and it _may_ be faster than
 		// iterating over the full array.
-		const { radius, size, score, sigma, lookupTable } = this;
+		const { size, score, sigma, lookupTable } = this;
 		const sigma2 = sigma * sigma;
+
+		// const radius = Math.floor( size / 2 );
+		const radius = this.radius;
 		const index = y * size + x;
 		const lookupWidth = 2 * radius + 1;
 		for ( let px = - radius; px <= radius; px ++ ) {
-			
+
 			for ( let py = - radius; py <= radius; py ++ ) {
-			
-				// const dist = Math.sqrt( x * x + y * y );
-				// const dist2 = dist * dist;
+
+				// const dist2 = px * px + py * py;
 				// const value = Math.E ** ( - dist2 / ( 2 * sigma2 ) );
-			
-				const lookupIndex = py * lookupWidth + px;
+
+				const lookupIndex = ( radius + py ) * lookupWidth + px + radius;
 				const value = lookupTable[ lookupIndex ];
-				
+
+				console.log(value);
 				let sx = ( x + px );
 				sx = sx < 0 ? size + sx : sx % size;
-				
+
 				let sy = ( y + py ) % size;
 				sy = sy < 0 ? size + sy : sy % size;
-				
+
 				const sindex = sy * size + sx;
 				score[ sindex ] += multiplier * value;
-				
+
 			}
-		
+
 		}
-		
+
 	}
-	
+
 	// TODO:
 	// - Use `setPointIndex` here?
 	// - Verify we're not inadvertantly removing a point that doesn't exist and vice versa.
 	addPointIndex( index ) {
 
 		this.binaryPattern[ index ] = 1;
+
 		const size = this.size;
 		const y = ~ ~ ( index / size );
 		const x = index - y * size;
@@ -164,7 +167,7 @@ export class BlueNoiseSamples {
 	}
 
 	addPoint( x, y ) {
-		
+
 		this.binaryPattern[ y * this.size + x ] = 1;
 		this.updateScore( x, y, 1 );
 		this.count ++;
@@ -172,23 +175,25 @@ export class BlueNoiseSamples {
 	}
 
 	removePointIndex( index ) {
-		
-		this.binaryPattern[ index ] = 1;
+
+		this.binaryPattern[ index ] = 0;
+
+		const size = this.size;
 		const y = ~ ~ ( index / size );
 		const x = index - y * size;
-		this.updateScore( x, y, 1 );
+		this.updateScore( x, y, - 1 );
 		this.count --;
-		
+
 	}
 
 	removePoint( x, y ) {
-	
+
 		this.binaryPattern[ y * this.size + x ] = 0;
 		this.updateScore( x, y, - 1 );
 		this.count --;
 
 	}
-	
+
 	copy( source ) {
 
 		this.resize( source.size );
