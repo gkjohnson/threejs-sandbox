e8fc77f3ada5eb2ffab6ddaea27cf6d57fd58ee1
Garrett Johnson <garrett.kjohnson@gmail.com>
2020-09-06 19:54:06 -0700

    Update utils.js

--- blue-noise-generation/src/utils.js
+++ blue-noise-generation/src/utils.js
@@ -1,3 +1,12 @@
-export function shuffleArray( array ) {
+function shuffleArray( array, random = Math.random ) {
+
+	for ( let i = array.length - 1, l = 0; i > 0; i -- ) {
+		
+		const replaceIndex = ~ ~ ( ( random() - 1e-6 ) * i );
+		const tmp = array[ i ];
+		array[ i ] = array[ replaceIndex ];
+		array[ replaceIndex ] = tmp;
+		
+	}
 
 }
