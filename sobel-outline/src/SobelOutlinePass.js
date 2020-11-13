be7a3f0c4da5d38bfd4e6deae57e8c8d7135df5a
Garrett Johnson <garrett.kjohnson@gmail.com>
2020-11-13 09:00:52 -0800

    update

--- sobel-outline/src/SobelOutlinePass.js
+++ sobel-outline/src/SobelOutlinePass.js
@@ -13,30 +13,32 @@ import {
 } from '//unpkg.com/three@0.114.0/build/three.module.js';
 import { Pass } from '//unpkg.com/three@0.114.0/examples/jsm/postprocessing/Pass.js';
 import { NormalPass } from '../../shader-replacement/src/passes/NormalPass.js';
-import { LinearDepthPass } from './LinearDepthPass.js';
+import { LinearDepthPass } from '../../gtaoPass/src/LinearDepthPass.js';
+import { RendererState } from '../../shader-replacement/src/RendererState.js';
+import { SobelOutlineShader } from './SobelOutlineShader.js';
 
 const rendererState = new RendererState();
 const blackColor = new Color( 0 );
 
-class SobelOutlinePass extends Pass {
+export class SobelOutlinePass extends Pass {
 
 	constructor( scene, camera ) {
-		
+
 		super();
 		this.enabled = true;
 		this.swap = true;
-		
+
 		this.scene = scene;
 		this.camera = camera;
-		
+
 		this.normalOutlineThickness = 1;
 		this.normalBias = 0;
 
 		this.depthOutlineThickness = 1;
 		this.depthOutlinePosition = 0;
 		this.depthBias = 0;
-		
-		this._depthReplacement = new LinearDepthReplacement();
+
+		this._depthReplacement = new LinearDepthPass();
 		this._depthBuffer =
 			new WebGLRenderTarget( 1, 1, {
 				minFilter: NearestFilter,
@@ -44,7 +46,7 @@ class SobelOutlinePass extends Pass {
 				format: RGBFormat,
 				type: HalfFloatType,
 			} );
-		
+
 		this._normalReplacement = new NormalPass();
 		this._normalBuffer =
 			new WebGLRenderTarget( 1, 1, {
@@ -52,14 +54,13 @@ class SobelOutlinePass extends Pass {
 				magFilter: NearestFilter,
 				format: RGBAFormat
 			} );
-		
-		this._compositeQuad = new Pass.FullScreenQuad( new ShaderMaterial( CompositeShader ) );
 
-		
+		this._compositeQuad = new Pass.FullScreenQuad( new ShaderMaterial( SobelOutlineShader ) );
+
 	}
 
 	render( renderer, writeBuffer, readBuffer ) {
-	
+
 		const finalBuffer = this.renderToScreen ? null : writeBuffer;
 		const {
 			scene,
@@ -67,13 +68,18 @@ class SobelOutlinePass extends Pass {
 
 			_depthBuffer,
 			_depthReplacement,
-			
+
 			_normalBuffer,
 			_normalReplacement,
-			
+
 			_compositeQuad,
+
+			normalOutlineThickness,
+			normalBias,
+			depthOutlineThickness,
+			depthBias,
 		} = this;
-		
+
 		rendererState.copy( renderer, scene );
 		const restoreOriginalValues = () => {
 
@@ -81,15 +87,38 @@ class SobelOutlinePass extends Pass {
 			_depthReplacement.reset( scene, true );
 
 		};
-		
+
+		scene.background = blackColor;
+
 		// render depth
-		
+		_depthReplacement.replace( scene, true, true );
+		renderer.setRenderTarget( _depthBuffer );
+		renderer.clear();
+		renderer.render( scene, camera );
+
 		// render normals
-		
+		_normalReplacement.replace( scene, true, false );
+		renderer.setRenderTarget( _normalBuffer );
+		renderer.clear();
+		renderer.render( scene, camera );
+
 		// composite final render
-		
+		// const compositeMaterial = _compositeQuad.material;
+		// compositeMaterial.uniforms.normalTex.value = _normalBuffer;
+		// compositeMaterial.uniforms.depthTex.value = _depthBuffer;
+
+		// compositeMaterial.uniforms.depthOutlineThickness.value = depthOutlineThickness;
+		// compositeMaterial.uniforms.depthBias.value = depthBias;
+		// compositeMaterial.uniforms.normalOutlineThickness.value = normalOutlineThickness;
+		// compositeMaterial.uniforms.normalBias.value = normalBias;
+		// renderer.getSize( compositeMaterial.uniforms.resolution.value );
+		// compositeMaterial.uniforms.resolution.value.multiplyScalar( renderer.getPixelRatio() );
+
+		// renderer.setRenderTarget( finalBuffer );
+		// _compositeQuad.render( renderer );
+
 		restoreOriginalValues();
-		
+
 	}
 
 	setSize( width, height ) {
---
+++ sobel-outline/src/SobelOutlineShader.js
@@ -0,0 +1,149 @@
+import { Vector2 } from '//unpkg.com/three@0.116.1/build/three.module.js';
+export const SobelOutlineShader = {
+
+	defines: {
+		ENABLE_BLUR: 1,
+		BLUR_RADIUS: 5,
+		DEPTH_THRESHOLD: '2e-3',
+		COLOR_HIT_ONLY: 0,
+	},
+
+	uniforms: {
+
+		intersectBuffer: { value: null },
+		sourceBuffer: { value: null },
+		packedBuffer: { value: null },
+		depthBuffer: { value: null },
+
+		intensity: { value: 0.5 },
+
+		renderSize: { value: new Vector2() },
+		marchSize: { value: new Vector2() },
+		blurStride: { value: 1 },
+
+	},
+
+	vertexShader:
+	/* glsl */`
+		varying vec2 vUv;
+		void main() {
+
+			vUv = uv;
+			gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+
+		}
+	`,
+
+	fragmentShader:
+	/* glsl */`
+		#include <common>
+		#include <packing>
+		#define E 2.7182818
+
+		varying vec2 vUv;
+		uniform sampler2D intersectBuffer;
+		uniform sampler2D sourceBuffer;
+		uniform sampler2D packedBuffer;
+		uniform sampler2D depthBuffer;
+
+		uniform vec2 renderSize;
+		uniform vec2 marchSize;
+		uniform float blurStride;
+
+		uniform float intensity;
+
+		// https://danielilett.com/2019-05-08-tut1-3-smo-blur/
+		// One-dimensional Gaussian curve function.
+		float gaussian( int x, int spread) {
+
+			float sigmaSqu = float( spread * spread );
+			return ( 1.0 / sqrt( 2.0 * PI * sigmaSqu ) ) * pow( E, - float( x * x ) / ( 2.0 * sigmaSqu ) );
+
+		}
+
+		vec3 UnpackNormal( vec4 d ) {
+
+			return d.xyz * 2.0 - 1.0;
+
+		}
+
+		void main() {
+
+			// Found, blending
+			vec4 source = texture2D( sourceBuffer, vUv );
+			vec3 sample = vec3( 0.0 );
+
+			#if ENABLE_BLUR
+
+			vec2 currTexel = vUv * renderSize;
+			vec2 currMarchTexel = vUv * marchSize;
+			vec2 texelRatio = marchSize / renderSize;
+
+			vec3 currNormal = UnpackNormal( texture2D( packedBuffer, vUv  ) );
+			float currDepth = texture2D( depthBuffer, vUv ).r;
+
+			float totalWeight = 1e-10;
+			int blurWidth = BLUR_RADIUS * 2 + 1;
+
+			for ( int x = - BLUR_RADIUS; x <= BLUR_RADIUS; x ++ ) {
+
+				for ( int y = - BLUR_RADIUS; y <= BLUR_RADIUS; y ++ ) {
+
+					// if ( x != 0 || y != 0 ) continue;
+
+					vec2 step = vec2( float( x ), float( y ) ) * float( blurStride );
+
+					// iterate over full res pixels
+					vec2 renderUv = currTexel + step;
+					renderUv /= renderSize;
+
+					// get the associated pixel in the AO buffer
+					vec2 marchUv = currMarchTexel + step * texelRatio;
+					marchUv /= marchSize;
+
+					// if the pixels are close enough in space then blur them together
+					float offsetDepth = texture2D( depthBuffer, renderUv ).r;
+					if ( abs( offsetDepth - currDepth ) <= 1e-1 ) {
+
+						// Weigh the sample based on normal similarity
+						vec3 offsetNormal = UnpackNormal( texture2D( packedBuffer, renderUv ) );
+						float weight = max( 0.0, dot( offsetNormal, currNormal ) - 0.9 ) * 10.0;
+
+						// gaussian distribution
+						weight *= gaussian( x, blurWidth ) * gaussian( y, blurWidth );
+
+						// accumulate
+						vec4 val = texture2D( intersectBuffer, marchUv );
+						sample += val.rgb * weight;
+						totalWeight += weight;
+
+					}
+
+				}
+
+			}
+
+			sample /= totalWeight;
+
+			#else
+
+			sample = texture2D( intersectBuffer, vUv ).rgb;
+
+			#endif
+
+			#if COLOR_HIT_ONLY
+
+			gl_FragColor = vec4( sample, 1.0 );
+
+			#else
+
+			source.rgb += sample * intensity;
+			gl_FragColor = source;
+
+			#endif
+
+
+		}
+	`
+
+};
